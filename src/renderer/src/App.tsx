6d9743ae40a03023367398fec29fd692a711b668
Felipe Gouvea <felipegalvesouvea@gmail.com>
2023-03-27 19:17:02 -0300

    chore: add tailwind css

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,3 +1,5 @@
+import './styles/globals.css'
+
 export function App() {
-  return <h1 style={{ fontFamily: 'DM Sans' }}>Hello Electron</h1>;
+  return <h1 className='text-blue-500'>Hello Electron</h1>;
 }
---
+++ src/renderer/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: 'DM Sans, sans-serif'
+      }
+    },
+  },
+  plugins: [],
+}
