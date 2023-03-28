89e3793fd45dbef7111ac0129b8d5605ba7ddda7
Felipe Gouvea <felipegalvesouvea@gmail.com>
2023-03-28 18:54:00 -0300

    style: create document page

--- src/renderer/src/pages/document.tsx
+++ src/renderer/src/pages/document.tsx
@@ -1,10 +1,23 @@
-import { Link } from "react-router-dom";
+import { ToC } from '../components/ToC'
 
 export function Document() {
   return (
-    <main className="flex-1 flex items-center justify-center text-flotes-400">
-      Document
-      <Link to="/">Acessar blank</Link>
+    <main className="flex-1 flex py-12 px-10 gap-8">
+      <aside className="hidden lg:block sticky top-0">
+        <span className="text-flotes-300 uppercase text-xs font-medium">Table of content</span>
+
+        <ToC.Root>
+          <ToC.Link>Back-end</ToC.Link>
+          <ToC.Section>
+            <ToC.Link>SOLID</ToC.Link>
+            <ToC.Link>Design Patterns</ToC.Link>
+          </ToC.Section>
+        </ToC.Root>
+      </aside>
+
+      <section className="flex-1 flex flex-col items-center">
+        
+      </section>
     </main>
   );
 }
