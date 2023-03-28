1a39b203cb1b4ae5f6c3772bb6dd7f4e7aa1ac09
Felipe Gouvea <felipegalvesouvea@gmail.com>
2023-03-28 19:39:25 -0300

    style: add animations for sidebar

--- src/renderer/tailwind.config.js
+++ src/renderer/tailwind.config.js
@@ -1,44 +1,59 @@
-const plugin = require('tailwindcss/plugin')
+const plugin = require("tailwindcss/plugin");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.tsx'
-  ],
+  content: ["./src/**/*.tsx"],
   theme: {
     extend: {
       fontFamily: {
-        sans: 'DM Sans, sans-serif'
+        sans: "DM Sans, sans-serif",
       },
       colors: {
         flotes: {
-          50: '#ebeaed',
-          100: '#c1bfc7',
-          200: '#a3a0ac',
-          300: '#797486',
-          400: '#5f596e',
-          500: '#37304a',
-          600: '#322c43',
-          700: '#272235',
-          800: '#1e1a29',
-          900: '#17141f',
+          50: "#ebeaed",
+          100: "#c1bfc7",
+          200: "#a3a0ac",
+          300: "#797486",
+          400: "#5f596e",
+          500: "#37304a",
+          600: "#322c43",
+          700: "#272235",
+          800: "#1e1a29",
+          900: "#17141f",
         },
+      },
+
+      keyframes: {
+        slideIn: {
+          from: { width: 0 },
+          to: { width: "var(--radix-collapsible-content-width)" },
+        },
+
+        slideOut: {
+          from: { width: "var(--radix-collapsible-content-width)" },
+          to: { width: 0 },
+        },
+      },
+
+      animation: {
+        slideIn: "slideIn 0.25s",
+        slideOut: "slideOut 0.25s",
       },
     },
   },
   plugins: [
-    require('@tailwindcss/typography'),
+    require("@tailwindcss/typography"),
 
     plugin(({ addUtilities }) => {
       addUtilities({
-        '.region-drag': {
-          '-webkit-app-region': 'drag'
+        ".region-drag": {
+          "-webkit-app-region": "drag",
         },
 
-        '.region-no-drag': {
-          '-webkit-app-region': 'no-drag'
-        }
-      })
-    })
+        ".region-no-drag": {
+          "-webkit-app-region": "no-drag",
+        },
+      });
+    }),
   ],
-}
+};
