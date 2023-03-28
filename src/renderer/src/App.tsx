ae056ab4a150c671a72fd5eeb54fb8d241fdbe43
Felipe Gouvea <felipegalvesouvea@gmail.com>
2023-03-27 22:51:59 -0300

    style: add home page

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,18 @@
+import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
 import './styles/globals.css'
 
 export function App() {
   return (
-    <div className="h-screen w-screnn bg-flotes-900 text-flotes-100"></div>
+    <div className="flex h-screen w-screnn bg-flotes-900 text-flotes-100">
+      <Sidebar />
+      <div className="flex-1 flex flex-col max-h-screen">
+        <Header />
+
+        <main className="flex-1 flex items-center justify-center text-flotes-400">
+          Selecione ou crie um documento
+        </main>
+      </div>
+    </div>
   )
 }
