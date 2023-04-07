fcb81f164981c84ed77956f8c4658448182804e6
Felipe Gouvea <felipegalvesouvea@gmail.com>
2023-04-07 00:31:45 -0300

    feat: structure of crud via IPC

--- src/shared/types/ipc.ts
+++ src/shared/types/ipc.ts
@@ -1,9 +1,27 @@
 export interface Document {
-  id: string
-  title: string
-  content: string
+  id: string;
+  title: string;
+  content?: string;
 }
 
+export interface FetchDocumentRequest {
+  id: string;
+}
+
+export interface DeleteDocumentRequest {
+  id: string;
+}
+
+export interface SaveDocumentRequest extends Document {}
+
 export interface FetchAllDocumentsResponse {
-  data: Document[]
+  data: Document[];
+}
+
+export interface FetchDocumentResponse {
+  data: Document;
+}
+
+export interface CreateDocumentResponse {
+  data: Document;
 }
